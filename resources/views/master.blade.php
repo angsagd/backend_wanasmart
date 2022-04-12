df878a64f478c4eae063fd8f410a95c81e8a76f4
Trisna Wirawan <64679698+trisnawirawan29@users.noreply.github.com>
2022-04-12 13:58:03 +0800

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -88,6 +88,7 @@
 	 
 	
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBocuQEfY1hThSb_AAefHNLPLCar31bjUE"></script>
+	<script src="https://unpkg.com/@popperjs/core@2"></script>
 
 	<!-- jQuery 3 -->
 	<script src="{{asset('assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js')}}"></script>
